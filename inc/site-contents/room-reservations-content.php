a9dbbf8efa0084a00d83cd260c8412961ee450cf
barnabasMatrai <matrai.barnabas1@gmail.com>
2024-11-17 23:22:10 +0100

    fix reservation error2

--- inc/site-contents/room-reservations-content.php
+++ inc/site-contents/room-reservations-content.php
@@ -6,19 +6,19 @@
 ?>
 
 <div class="d-flex justify-content-center">
-    <ul>
-        <li>
-            <div>
-                <?php
-                if (isset($_SESSION['arrival'])) {
-                    echo '<p>Anreise: ' . $_SESSION['arrival'] . '</p>
-                    <p>Abreise: ' . $_SESSION['departure'] . '</p>
-                    <p>' . ($_SESSION['breakfast'] ? 'Mit Frühstück' : 'Ohne Frühstück') . '</p>
-                    <p>' . ($_SESSION['parking'] ? 'Mit Parkplatz' : 'Ohne Parkplatz') . '</p>
-                    <p>' . ($_SESSION['cat'] ? 'Katze' : 'Keine Katze') . '</p>';
-                }
-                ?>
-            </div>
-        </li>
-    </ul>
+    <?php
+    if (isset($_SESSION['arrival'])) {
+        echo '<ul>
+                  <li>
+                      <div>
+                            <p>Anreise: ' . $_SESSION['arrival'] . '</p>
+                            <p>Abreise: ' . $_SESSION['departure'] . '</p>
+                            <p>' . ($_SESSION['breakfast'] ? 'Mit Frühstück' : 'Ohne Frühstück') . '</p>
+                            <p>' . ($_SESSION['parking'] ? 'Mit Parkplatz' : 'Ohne Parkplatz') . '</p>
+                            <p>' . ($_SESSION['cat'] ? 'Katze' : 'Keine Katze') . '</p>
+                      </div>
+                  </li>
+              </ul>';
+    }
+    ?>
 </div>
