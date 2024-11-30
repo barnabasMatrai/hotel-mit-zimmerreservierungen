2eac56eb85d412c59ac9644760aed22cd9ddc1de
barnabasMatrai <matrai.barnabas1@gmail.com>
2024-11-30 18:41:34 +0100

    fix room reservation

--- inc/site-contents/room-reservations-content.php
+++ inc/site-contents/room-reservations-content.php
@@ -13,8 +13,8 @@
                       <div>
                             <p>Anreise: ' . $_SESSION['arrival'] . '</p>
                             <p>Abreise: ' . $_SESSION['departure'] . '</p>
-                            <p>' . ($_SESSION['breakfast'] ? 'Mit Frühstück' : 'Ohne Frühstück') . '</p>
-                            <p>' . ($_SESSION['parking'] ? 'Mit Parkplatz' : 'Ohne Parkplatz') . '</p>
+                            <p>' . ($_SESSION['breakfast'] === "withbreakfast" ? 'Mit Frühstück' : 'Ohne Frühstück') . '</p>
+                            <p>' . ($_SESSION['parking'] === "withoutbreakfast" ? 'Mit Parkplatz' : 'Ohne Parkplatz') . '</p>
                             <p>' . ($_SESSION['cat'] ? 'Katze' : 'Keine Katze') . '</p>
                       </div>
                   </li>
