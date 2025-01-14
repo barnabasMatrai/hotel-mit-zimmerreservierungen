2d84ae220b787f0512f7c0ed24dcf9b615b97ca6
barnabasMatrai <matrai.barnabas1@gmail.com>
2025-01-14 14:20:37 +0100

    show date for articles and some style modifications

--- inc/site-contents/articles-content.php
+++ inc/site-contents/articles-content.php
@@ -11,12 +11,17 @@ $articles = getArticles();
 ?>
 
 <div class="d-flex justify-content-center">
-    <ul>
+    <ul class="list-unstyled">
         <?php foreach ($articles as $article):
-                if (file_exists($dir . $article -> Filename)): ?>
-        <li><div class='border border-3 border-black m-2'>
-            <img src="<?= $dir . $article -> Filename ;?>" alt='Bild'>
-            <p><?= $article -> Comment ;?></p>
+                if (file_exists($dir . $article -> Filename)):
+                    $path_parts = pathinfo($article -> Filename);
+                    $filenameWithoutExtension = $path_parts['filename']; ?>
+        <li>
+            <div class='border border-3 border-black m-2 text-center'>
+                <p><?= $article -> UploadDate ?></p>
+                <img class="border-top border-bottom border-3 border-black" src="<?= $dir . $article -> Filename ;?>" alt='<?= $filenameWithoutExtension ?>'>
+                <p><?= $article -> Comment ;?></p>
+            </div>
         </li>
         <?php   endif;
               endforeach; ?>
